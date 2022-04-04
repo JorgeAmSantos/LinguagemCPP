90c8f257b947553f44fdee58535a3ce604a0fd83
Jorge Antonio <moxruby00001@gmail.com>
2022-04-04 09:15:12 -0300

    C++

---
+++ 1-01042022/segundo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n1 = 0, n2 = 0, n3 = 0;
+    std::cout << "Entre com um número\n";
+        std::cin>> n1;
+    std::cout << "Entre com um número\n";
+        std::cin>> n2;
+    n3 = n1 + n2;
+    std::cout <<"O resultado: "<<n3<<"\n";
+
+    return 0;
+}
